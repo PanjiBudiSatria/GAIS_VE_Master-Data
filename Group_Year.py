714f94a991a9cb3065f35778788f275f379a1c4d
Panji Budi Satria <panjibudi467@gmail.com>
2022-10-19 13:45:13 +0700

    CRUD

--- Group_Year.py
+++ Group_Year.py
@@ -100,46 +100,46 @@ class TestGroupYear(unittest.TestCase):
         self.assertEqual(response_alert,"Successfully to update the data")
         self.assertEqual(response_data,"A/Y 2023 Rev1")
 
-    # def test_c_price_points_delete(self):
-    #     driver=self.driver
-    #     driver.maximize_window()
-    #     driver.get("http://10.9.98.65/gais65/")
-    #     time.sleep(3)
-    #     driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div/form/input[4]").send_keys("it.appsupport")
-    #     time.sleep(1)
-    #     driver.find_element(By.ID,"regularInput").send_keys("Gais432")
-    #     time.sleep(1)
-    #     driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div/form/div/button").click()
-    #     time.sleep(3)
-    #     driver.find_element(By.XPATH,"//input[@id='14']").click()
-    #     time.sleep(3)
-    #     driver.find_element(By.ID,"save").click()
-    #     time.sleep(5)
+    def test_c_group_year_delete(self):
+        driver=self.driver
+        driver.maximize_window()
+        driver.get("http://10.9.98.65/gais65/")
+        time.sleep(3)
+        driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div/form/input[4]").send_keys("it.appsupport")
+        time.sleep(1)
+        driver.find_element(By.ID,"regularInput").send_keys("Gais432")
+        time.sleep(1)
+        driver.find_element(By.XPATH,"/html/body/div[2]/div[6]/div/div/form/div/button").click()
+        time.sleep(3)
+        driver.find_element(By.XPATH,"//input[@id='14']").click()
+        time.sleep(3)
+        driver.find_element(By.ID,"save").click()
+        time.sleep(5)
 
-    #     driver.find_element(By.XPATH,"//a[@data-id='427']").click()
-    #     time.sleep(2)
+        driver.find_element(By.XPATH,"//a[@data-id='427']").click()
+        time.sleep(2)
 
-    #     driver.find_element(By.XPATH,"//img[@src='assets/images/btn/button-master-data.gif']").click()
-    #     time.sleep(2)
+        driver.find_element(By.XPATH,"//img[@src='assets/images/btn/button-master-data.gif']").click()
+        time.sleep(2)
 
-    #     driver.find_element(By.XPATH,"//img[@src='assets/images/btn/target-list.png']").click()
-    #     time.sleep(1)
+        driver.find_element(By.XPATH,"//img[@src='assets/images/btn/target-list.png']").click()
+        time.sleep(1)
 
-    #     driver.find_element(By.XPATH,"/html/body/div[4]/div/div/div/ul/li/a[1]/img").click()
-    #     time.sleep(2)
+        driver.find_element(By.XPATH,"/html/body/div[4]/div/div/div/ul/li/a[7]/img").click()
+        time.sleep(2)
 
-    #     driver.find_element(By.XPATH,"/html/body/div[4]/div/form/table/tbody/tr[1]/td/input").send_keys("D")
-    #     time.sleep(1)
-    #     driver.find_element(By.XPATH,"/html/body/div[4]/div/form/table/tbody/tr[3]/td/button").click()
-    #     time.sleep(1)
+        driver.find_element(By.XPATH,"/html/body/div[4]/div/form/table/tbody/tr[1]/td/input").send_keys("A/Y 2023 Rev1")
+        time.sleep(1)
+        driver.find_element(By.XPATH,"/html/body/div[4]/div/form/table/tbody/tr[2]/td/button").click()
+        time.sleep(1)
 
-    #     driver.find_element(By.XPATH,"//a[@title='Delete']").click()
-    #     time.sleep(1)
-    #     driver.find_element(By.XPATH,"//input[@value='OK']").click()
-    #     time.sleep(2)
+        driver.find_element(By.XPATH,"//a[@title='Delete']").click()
+        time.sleep(1)
+        driver.find_element(By.XPATH,"//input[@value='OK']").click()
+        time.sleep(2)
 
-    #     response_data = driver.find_element(By.XPATH,"/html/body/div[4]/div/div/table/tbody/tr/td").text
-    #     self.assertEqual(response_data,"-- Data not found --")
+        response_data = driver.find_element(By.XPATH,"/html/body/div[4]/div/div[2]/table/tbody/tr/td").text
+        self.assertEqual(response_data,"-- Data not found --")
 
 
     def tearDown(self):
